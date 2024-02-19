8693ab3bc502c248107c7a747fbd4c83d15e0156
[firstname lastname] <[valid-email]>
2024-02-19 11:29:29 +0300

    new2

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -66,5 +66,5 @@ List<Course> courses = courseManager.GetAll();
 for (int i = 0; i < courses.Count; i++)
 {
 
-    Console.WriteLine(courses[i].Name + "/" + courses[i].Price);
+    Console.WriteLine(courses[i].Name + "/ " + courses[i].Price);
 }
